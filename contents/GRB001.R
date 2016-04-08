e5cc3f0fa2f7b898d87e576aa40719f000d68c99
Satoshi Takahama <satoshi.takahama@gmail.com>
2016-04-08 02:31:11 +0200

    x-axis rotation in time scale for 04 and 09

--- contents/GRB001.R
+++ contents/GRB001.R
@@ -49,58 +49,58 @@ ColClasses <- function(x) {
   classes
 }
 
-
-dixon.test <- function (x, type = 0, opposite = FALSE, two.sided = TRUE) {
-  ## from the outlier library
-  DNAME <- deparse(substitute(x))
-  x <- sort(x[complete.cases(x)])
-  n <- length(x)
-  if ((type == 10 || type == 0) & (n < 3 || n > 30)) 
-    stop("Sample size must be in range 3-30")
-  if (type == 11 & (n < 4 || n > 30)) 
-    stop("Sample size must be in range 4-30")
-  if (type == 12 & (n < 5 || n > 30)) 
-    stop("Sample size must be in range 5-30")
-  if (type == 20 & (n < 4 || n > 30)) 
-    stop("Sample size must be in range 4-30")
-  if (type == 21 & (n < 5 || n > 30)) 
-    stop("Sample size must be in range 5-30")
-  if (type == 22 & (n < 6 || n > 30)) 
-    stop("Sample size must be in range 6-30")
-  if (sum(c(0, 10, 11, 12, 20, 21, 22) == type) == 0) 
-    stop("Incorrect type")
-  if (type == 0) {
-    .type <- c(10, 11, 21, 22)
-    type <- .type[1+findInterval(n, c(7, 10, 13), TRUE)]
+if(!require(outliers))
+  dixon.test <- function (x, type = 0, opposite = FALSE, two.sided = TRUE) {
+    ## from the outlier library
+    DNAME <- deparse(substitute(x))
+    x <- sort(x[complete.cases(x)])
+    n <- length(x)
+    if ((type == 10 || type == 0) & (n < 3 || n > 30)) 
+      stop("Sample size must be in range 3-30")
+    if (type == 11 & (n < 4 || n > 30)) 
+      stop("Sample size must be in range 4-30")
+    if (type == 12 & (n < 5 || n > 30)) 
+      stop("Sample size must be in range 5-30")
+    if (type == 20 & (n < 4 || n > 30)) 
+      stop("Sample size must be in range 4-30")
+    if (type == 21 & (n < 5 || n > 30)) 
+      stop("Sample size must be in range 5-30")
+    if (type == 22 & (n < 6 || n > 30)) 
+      stop("Sample size must be in range 6-30")
+    if (sum(c(0, 10, 11, 12, 20, 21, 22) == type) == 0) 
+      stop("Incorrect type")
+    if (type == 0) {
+      .type <- c(10, 11, 21, 22)
+      type <- .type[1+findInterval(n, c(7, 10, 13), TRUE)]
+    }
+    if (xor(((x[n] - mean(x)) < (mean(x) - x[1])), opposite)) {
+      alt <- paste("lowest value", x[1], "is an outlier")
+      Q <- switch(sprintf("%d", type),
+                  "10"=(x[2] - x[1])/(x[n] - x[1]),
+                  "11"=(x[2] - x[1])/(x[n - 1] - x[1]),
+                  "12"=(x[2] - x[1])/(x[n - 2] - x[1]),
+                  "20"=(x[3] - x[1])/(x[n] - x[1]),
+                  "21"=(x[3] - x[1])/(x[n - 1] - x[1]),
+                  (x[3] - x[1])/(x[n - 2] - x[1]))
+    }
+    else {
+      alt <- paste("highest value", x[n], "is an outlier")
+      Q <- switch(sprintf("%d", type),
+                  "10"=(x[n] - x[n - 1])/(x[n] - x[1]),
+                  "11"=(x[n] - x[n - 1])/(x[n] - x[2]),
+                  "12"=(x[n] - x[n - 1])/(x[n] - x[3]),
+                  "20"=(x[n] - x[n - 2])/(x[n] - x[1]),
+                  "21"=(x[n] - x[n - 2])/(x[n] - x[2]),
+                  (x[n] - x[n - 2])/(x[n] - x[3]))
+    }
+    pval <- pdixon(Q, n, type)
+    if (two.sided) {
+      pval <- 2 * pval
+      if (pval > 1) 
+        pval <- 2 - pval
+    }
+    RVAL <- list(statistic = c(Q = Q), alternative = alt, p.value = pval, 
+                 method = "Dixon test for outliers", data.name = DNAME)
+    class(RVAL) <- "htest"
+    return(RVAL)
   }
-  if (xor(((x[n] - mean(x)) < (mean(x) - x[1])), opposite)) {
-    alt <- paste("lowest value", x[1], "is an outlier")
-    Q <- switch(sprintf("%d", type),
-                "10"=(x[2] - x[1])/(x[n] - x[1]),
-                "11"=(x[2] - x[1])/(x[n - 1] - x[1]),
-                "12"=(x[2] - x[1])/(x[n - 2] - x[1]),
-                "20"=(x[3] - x[1])/(x[n] - x[1]),
-                "21"=(x[3] - x[1])/(x[n - 1] - x[1]),
-                (x[3] - x[1])/(x[n - 2] - x[1]))
-  }
-  else {
-    alt <- paste("highest value", x[n], "is an outlier")
-    Q <- switch(sprintf("%d", type),
-                "10"=(x[n] - x[n - 1])/(x[n] - x[1]),
-                "11"=(x[n] - x[n - 1])/(x[n] - x[2]),
-                "12"=(x[n] - x[n - 1])/(x[n] - x[3]),
-                "20"=(x[n] - x[n - 2])/(x[n] - x[1]),
-                "21"=(x[n] - x[n - 2])/(x[n] - x[2]),
-                (x[n] - x[n - 2])/(x[n] - x[3]))
-  }
-  pval <- pdixon(Q, n, type)
-  if (two.sided) {
-    pval <- 2 * pval
-    if (pval > 1) 
-      pval <- 2 - pval
-  }
-  RVAL <- list(statistic = c(Q = Q), alternative = alt, p.value = pval, 
-               method = "Dixon test for outliers", data.name = DNAME)
-  class(RVAL) <- "htest"
-  return(RVAL)
-}
